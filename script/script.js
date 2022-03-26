62f2505e4b43042b18655dc79bc2d734bf3d2b1d
Lucas R. de Souza <LucasRSouza2000@outlook.com>
2022-03-26 06:41:29 -0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,4 +1,4 @@
-
+'use strict';
 
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
@@ -12,3 +12,33 @@ window.onclick = function(e) {
       }
     }
 }
+
+const overlay = document.querySelector('.overlay');
+const aboutMe = document.querySelector('.aboutMe');
+const showModal = document.querySelector('.modal');
+const btnModal = document.querySelector('.close-modal');
+
+function overlayShow() {
+ overlay.classList.remove('hidden');
+}
+
+function overlayHidden() {
+  overlay.classList.add('hidden');
+  showModal.classList.add('hidden');
+}
+
+function modalShow() {
+  showModal.classList.remove('hidden');
+}
+
+function modalHidden() {
+  showModal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+aboutMe.addEventListener('click', modalShow);
+btnModal.addEventListener('click', modalHidden)
+aboutMe.addEventListener('click', overlayShow);
+overlay.addEventListener('click', overlayHidden)
+
+
